8869e70368a39a1f5d870fd4e87db2a05235e14b
fonzerelly <christian.hoerauf@gmail.com>
2020-08-23 14:38:35 +0200

    implements error handling

--- src/Bowling.elm
+++ src/Bowling.elm
@@ -32,9 +32,19 @@ roll pins game =
             StartedFrame fst -> FinalizedFrame (fst, pins)
             _ -> frame
 
+        frameCount = game 
+            |> extractFrames
+            |> List.length
+
         newBowlingGame = game
             |> extractFrames
             |> initFrames
             |> BowlingGame
     in
-    Ok <| newBowlingGame
+        if (pins > 10) || (pins < 0) then
+            Err <| String.append "Invalid number of Pins: " <| String.fromInt pins
+        else
+            if frameCount > 10 then
+                Err "It is not allowed roll more than 10 frames!"
+            else
+                Ok <| newBowlingGame
--- tests/BowlingTests.elm
+++ tests/BowlingTests.elm
@@ -5,6 +5,7 @@ import Expect
 import Bowling exposing (BowlingGame(..), roll, Frame(..))
 import Debug
 import Html exposing (a)
+import Result
 
 initialGame = BowlingGame []
 
@@ -12,7 +13,10 @@ repeatedRoll: Int -> Result String BowlingGame
 repeatedRoll count = if count == 0 then
                         roll 0 initialGame
                      else
-                        Result.map (roll count) (repeatedRoll (count - 1))
+                        let
+                            pins = min count 10
+                        in
+                        Result.map (roll pins) (repeatedRoll (count - 1))
                             |> Result.andThen identity 
 
 all: Test
@@ -52,9 +56,44 @@ all = describe "Bowling"
                         result = repeatedRoll 4
                     in
                         result |> Expect.equal expectedResult
+            
+            , test "should error on too many pins" <|
+                \_ ->  roll 11 initialGame |> Expect.err
+
+            , test "should error on too few pins" <|
+                \_ ->  roll -1 initialGame |> Expect.err
+            , test "should error on too much Frames" <|
+                \_ -> 
+                    let
+                        result = repeatedRoll 21
+                    in
+                        result |> Expect.err
+            -- , test "should allow three rolls on strike in 10th frame" <|
+            --     \_ -> let
+            --             lastFrame frames = case frames of
+            --                []   -> Bowling.EmptyFrame
+            --                [f]  -> f
+            --                _::l -> lastFrame l
+
+            --             expectedResult = TenthFrame (10,0,0)
+            --             gameAt10thFrame = repeatedRoll 17
+
+            --             strike = 10
+
+            --             resultingGame = Result.map (roll strike) gameAt10thFrame
+            --                 |> Result.andThen identity
+
+            --             tenthFrame case resultingGame of
+            --                Bowling.BowlingGame frames -> lastFrame frames                       
+                        
+            --           in
+            --             Err "doof" |> Expect.err
             -- should work now in general
-            -- missing case: pins > 9
             -- missing case Tenth Frame
+
+                    -- if first throw in tenth frame is a strike
+                    -- or if the finalizedFrame endsup to 10 then we
+                    -- need a third roll
             
             ]
         ]
